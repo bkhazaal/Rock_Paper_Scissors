7557c448bbcd80c86741db89e9ffc9e65fb2e9c6
Ben Khazaal <thajukez101@gmail.com>
2024-01-18 11:38:28 -0700

    firstcommit

---
+++ Website.js
@@ -0,0 +1,8 @@
+let PlayerScore = 0;
+let ComputerScore = 0;
+const options = ["rock", "paper", "scissors"];
+
+function ComputerTurn() {
+  let Index = Math.floor(Math.random() * 3);
+  return choice[Index];
+}
